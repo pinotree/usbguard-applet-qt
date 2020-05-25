a83558ac8849a4614e909fc0feb859dc5d437ae3
Pino Toscano <toscano.pino@tiscali.it>
2020-05-25 23:11:18 +0200

    Amend D-Bus XML with Qt types
    Create custom typedefs for the custom types used in the D-Bus
    interfaces, so they can be handled by QtDBus.

---
+++ DBusTypes.h
@@ -0,0 +1,28 @@
+//
+// Copyright (C) 2020 Pino Toscano
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authors: Pino Toscano <toscano.pino@tiscali.it>
+//
+#pragma once
+
+#include <QList>
+#include <QMap>
+#include <QPair>
+#include <QString>
+
+typedef QPair<unsigned int, QString> DBusRule;
+typedef QList<DBusRule> DBusRules;
+typedef QMap<QString, QString> DBusAttributes;
